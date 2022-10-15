1c70cdc974f97f00b0ac256f282a2d48a4806a3f
Katie Min <katie.h.min@gmail.com>
2022-10-15 16:09:32 -0700

    added a caches table

--- app.js
+++ app.js
@@ -20,6 +20,21 @@ const User = sequelize.define("users", {
   },
 });
 
+// Define the Cache model for the "caches" table.
+const Cache = sequelize.define("caches", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  location: {
+    type: Sequelize.STRING,
+  },
+  message: {
+    type: Sequelize.STRING,
+  }
+});
+
 // Create the "users" table.
   User.sync({
     force: true,
@@ -42,11 +57,11 @@ const User = sequelize.define("users", {
       ]);
     })
     .then(function () {
-      // Retrieve accounts.
+      // Retrieve users.
       return User.findAll();
     })
     .then(function (users) {
-      // Print out the balances.
+      // Print out the ids and names.
       users.forEach(function (users) {
         console.log(users.id + " " + users.name);
       });
@@ -56,3 +71,37 @@ const User = sequelize.define("users", {
       console.error("error: " + err.message);
       process.exit(1);
     });
+
+
+    // Create the "caches" table.
+      Cache.sync({
+        force: true,
+      })
+        .then(function () {
+          // Insert four rows into the "caches" table.
+          return Cache.bulkCreate([
+            {location: "Times Square, New York",
+             message: "The place to be on New Years'!"},
+            {location: "Huntington Library, Los Angeles",
+             message: "Beautiful place... took a family trip here and it was absolutely wonderful."},
+            {location: "",
+             message: ""},
+            {location: "",
+             message: ""},
+          ]);
+        })
+        .then(function () {
+          // Retrieve caches.
+          return Cache.findAll();
+        })
+        .then(function (caches) {
+          // Print out the ids, locations, and messages.
+          caches.forEach(function (caches) {
+            console.log(caches.id + " " + caches.location + " " + caches.message);
+          });
+          process.exit(0);
+        })
+        .catch(function (err) {
+          console.error("error: " + err.message);
+          process.exit(1);
+        });
--- dbinit.sql
+++ dbinit.sql
@@ -2,3 +2,9 @@ CREATE TABLE users (
     id UUID PRIMARY KEY,
     name STRING
 );
+
+CREATE TABLE caches (
+    id INTEGER PRIMARY KEY, 
+    location STRING, 
+    message STRING
+);
