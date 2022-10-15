e2c607d9f9e2f2acee6becf182daf2b5cfda7f50
Shiwei Chen <april.chen.0615@gmail.com>
2022-10-15 15:52:28 -0700

    Added Tailwind CSS skeleton
    Added Tailwind CSS functionality into this build, I will improve upon it later!

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import NameForm from './NameForm/NameForm';
 import Map from './Map/Map';
-
+import './index.css';
 
 const App = () => {
     return (
         <div>
-            <h1>Welcome to DigiCache!</h1>
-            <h4>What's your name?</h4>
+            <div class="shadow-md m-3 p-3 rounded-md">
+                <h1 class="text-4xl text-center text-gray-700">Welcome to DigiCache!</h1>
+            </div>
+            <div class="m-2 p-5">
+                <h4 class="text-3xl text-center">What's your name?</h4>
+            </div>
             <NameForm />
             <Map />
         </div>
